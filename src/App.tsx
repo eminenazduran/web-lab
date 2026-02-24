27708b068def4974408444919c4d816219cd6c95
Emine <emine@example.com>
2026-02-24 22:39:46 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Emine</p>
+      <p>Ogrenci No: 123456789</p>
+    </div>
+  )
+}
+
+export default App
